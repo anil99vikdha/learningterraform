e47a294f2bebe2a1ddac5274b9dd03d0302f0d02
Anil Kumar Maheswarla <anilkumar.mah999@gmail.com>
2025-09-28 05:02:39 +0000

    Initial code

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+  default     = "tf-az-resource-group"
+}
+
+variable "location" {
+  description = "The Azure region where resources will be created"
+  type        = string
+  default     = "West Europe"
+}
