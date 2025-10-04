7e07a2f406231683c304cba2821ed7ea17b79446
Anil Kumar Maheswarla <anilkumar.mah999@gmail.com>
2025-10-04 04:22:07 +0000

    added debug log

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "resource_group_name" {
   description = "The name of the resource group"
   type        = string
-  default     = "tf-az-resource-group-tfcloud-dynamic"
+  default     = "tf-az-resource-group-tfcloud-dynamic-debug"
 }
 
 variable "location" {
