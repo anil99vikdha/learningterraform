b77ea27a4f92fe56c024ad06fe9aa3456119331a
Anil Kumar Maheswarla <anilkumar.mah999@gmail.com>
2025-10-04 11:10:57 +0000

    testing dynamic creds

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "resource_group_name" {
   description = "The name of the resource group"
   type        = string
-  default     = "app2"
+  default     = "app3"
 }
 
 variable "location" {
