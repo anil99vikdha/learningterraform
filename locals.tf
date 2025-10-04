036b81a0108fe028ff1f7d170335efdc76d830ad
Anil Kumar Maheswarla <anilkumar.mah999@gmail.com>
2025-10-04 05:28:17 +0000

    added conditioon

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
 locals {
   rg_base_name = "tf-az-resource-group"
-  rg_name      = var.gen_rand_id ? "${local.rg_base_name}-${var.resource_group_name}-${random_id.rg_rand_id.hex}" : "${local.rg_base_name}-${var.resource_group_name}-${var.environment}"
+  rg_name      = var.gen_rand_id ? "${local.rg_base_name}-${var.resource_group_name}-${random_id.rg_rand_id[0].hex}" : "${local.rg_base_name}-${var.resource_group_name}-${var.environment}"
 }
