c1085bd663c231240eed46bb1dbc1b1dc90b00a8
Anil Kumar Maheswarla <anilkumar.mah999@gmail.com>
2025-10-04 12:06:52 +0000

    adjusted dynamic creds

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "resource_group_name" {
   description = "The name of the resource group"
   type        = string
-  default     = "app3"
+  default     = "app2"
 }
 
 variable "location" {
