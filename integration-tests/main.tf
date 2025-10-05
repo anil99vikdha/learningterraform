f963941a9dbd745b16d400109f8d31078b7e6c0a
Anil Kumar Maheswarla <anilkumar.mah999@gmail.com>
2025-10-05 04:54:57 +0000

    changed rg name

--- integration-tests/main.tf
+++ integration-tests/main.tf
@@ -1,5 +1,5 @@
 module "rg_testing" {
   source              = "git::https://github.com/anil99vikdha/learningterraform.git//"
-  resource_group_name = "app2"
+  resource_group_name = "app3"
   gen_rand_id         = "xe54"
 }
